32d546e852a097f068338337b4f9392456c1f028
hunny <hunnyjain711@gmail.com>
2024-01-12 00:54:39 +0530

    Added the test DB config.

--- rate_shopping/settings/base.py
+++ rate_shopping/settings/base.py
@@ -111,6 +111,13 @@ DATABASES = {
     }
 }
 
+TEST = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "test_db.sqlite3",
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
